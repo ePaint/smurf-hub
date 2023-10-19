7119c3faeb0595dac6c2947992d0722babab9ba8
sebastian.rossetti <sebastian.rossetti@piano.io>
2023-10-19 23:18:38 +0200

    Forced KeePass used, improved integration

--- sources/keepass.py
+++ sources/keepass.py
@@ -87,6 +87,7 @@ class KeePass:
             return 'test'
 
         try:
+            message_popup(message=KEEPASS_CREATE_PATH)
             self.database = create_database(KEEPASS_CREATE_PATH, password=self.master_key)
             message_popup(message=self.database.filename)
             self.group = self.database.add_group(self.database.root_group, APP_TITLE, icon='1')
