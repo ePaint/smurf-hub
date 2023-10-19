6529899329d4949532bda51bd8b8abf90fb9f965
sebastian.rossetti <sebastian.rossetti@piano.io>
2023-10-19 23:27:17 +0200

    Forced KeePass used, improved integration

--- sources/keepass.py
+++ sources/keepass.py
@@ -1,7 +1,11 @@
 import os
+import shutil
 from enum import Enum
 from pathlib import Path
 from typing import Optional
+
+from pykeepass.kdbx_parsing import KDBX
+
 from sources.settings import SETTINGS
 from pykeepass import PyKeePass, create_database
 from pykeepass.entry import Entry
@@ -90,6 +94,21 @@ class KeePass:
         try:
             message_popup(message=KEEPASS_CREATE_PATH)
             self.database = PyKeePass(KEEPASS_CREATE_PATH, password=self.master_key)
+
+            # save to temporary file to prevent database clobbering
+            # see issues 223, 101
+            filename_tmp = Path(KEEPASS_CREATE_PATH).with_suffix('.tmp')
+            try:
+                KDBX.build_file(
+                    self.database,
+                    filename_tmp,
+                    password=self.master_key,
+                )
+            except Exception as e:
+                os.remove(filename_tmp)
+                raise e
+            shutil.move(filename_tmp, KEEPASS_CREATE_PATH)
+            
             # self.database = create_database(Path(KEEPASS_CREATE_PATH), password=self.master_key)
             message_popup(message=self.database.filename)
             self.group = self.database.add_group(self.database.root_group, APP_TITLE, icon='1')
